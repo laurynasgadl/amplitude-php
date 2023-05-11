17888522ca7174c5329c22b7a57e7b0371987671
ugniussmaizys <40465799+ugnsma@users.noreply.github.com>
2023-05-11 11:30:32 +0300

    feat: add V2 implementation (#6)
    Add Amplitude V2 implementation

---
+++ src/AbstractAmplitude.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Luur\Amplitude;
+
+use GuzzleHttp\Client;
+use JsonSerializable;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
+
+abstract class AbstractAmplitude
+{
+    protected string $apiKey;
+    protected ClientInterface $client;
+
+    public function __construct(string $apiKey, ClientInterface $client = null)
+    {
+        $this->apiKey = $apiKey;
+        $this->client = $client ?? $this->getDefaultClient();
+    }
+
+    abstract public function send(JsonSerializable $message): ResponseInterface;
+
+    public function setClient(ClientInterface $client): void
+    {
+        $this->client = $client;
+    }
+
+    protected function getDefaultClient(): ClientInterface
+    {
+        return new Client([
+            'base_uri' => $this->getBaseURI(),
+            'timeout' => 60.0,
+        ]);
+    }
+
+    abstract public function getBaseURI(): string;
+}
